e4cbf7595891ae56e5c06d70d66ec94b2758418f
shubham <shubham763116@gmail.com>
2021-05-22 13:33:03 +0530

    jss makeStyles

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SideMenu from '../Components/SideMenu';
+
+
+function App() {
+  return (
+    <>
+      <SideMenu/>
+      <div>here we go</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/SideMenu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { makeStyles, withStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    sideMenu: {
+        display: 'flex',
+        flexDirection:'column',
+        position: 'absolute',
+        left: '0px',
+        width: '320px',
+        height: '100%',
+        backgroundColor: '#253053'
+        
+    }
+});
+
+export default function SideMenu() {
+    const classes = useStyles();
+    console.log(classes);
+    return (
+        <div className={classes.sideMenu}>
+            
+        </div>
+    )
+}
